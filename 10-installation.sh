f61630ebb5d267caddddc28add1958093ca2b000
Ashok <c00540105@gmail.com>
2026-03-14 18:26:48 -0500

    Installing Nginx

--- 10-installation.sh
+++ 10-installation.sh
@@ -15,3 +15,21 @@ else
     echo "Nginx installation . . . FAILURE"
     exit 1
 fi
+
+dnf install mysql -y
+
+if [ $? -eq 0 ]; then
+    echo "MySQL installed . . . SUCCESS"
+else
+    echo "MySQL installation . . . FAILURE"
+    exit 1
+fi
+
+dnf install nodejs -y
+
+if [ $? -eq 0 ]; then
+    echo "Node.js installed . . . SUCCESS"
+else
+    echo "Node.js installation . . . FAILURE"
+    exit 1
+fi

5e8df9f294731846a45e8a63d7a54c72e54a8906
Ashok <c00540105@gmail.com>
2026-03-14 18:49:42 -0500

    Installing Nginx

--- 10-installation.sh
+++ 10-installation.sh
@@ -6,23 +6,31 @@ if [ $USERID -ne 0 ]; then
     exit 1
 fi
 
-# By default shell will not execute, only execute when called
-VALIDATE() {
-    if [ $? -eq 0 ]; then
-        echo "$1 installed . . . SUCCESS"
-    else
-        echo "$1 installation . . . FAILURE"
-        exit 1
-    fi
-}
 
 echo "Installing Nginx web server..."
 dnf install nginx -y
-VALIDATE "Nginx"
 
+if [ $? -eq 0 ]; then
+    echo "Nginx installed . . . SUCCESS"
+else
+    echo "Nginx installation . . . FAILURE"
+    exit 1
+fi
 
 dnf install mysql -y
-VALIDATE "MySQL"
+
+if [ $? -eq 0 ]; then
+    echo "MySQL installed . . . SUCCESS"
+else
+    echo "MySQL installation . . . FAILURE"
+    exit 1
+fi
 
 dnf install nodejs -y
-VALIDATE "Node.js"
+
+if [ $? -eq 0 ]; then
+    echo "Node.js installed . . . SUCCESS"
+else
+    echo "Node.js installation . . . FAILURE"
+    exit 1
+fi

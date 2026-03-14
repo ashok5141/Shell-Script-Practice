c10c06f507678abd916830ac61ea15dd4f750561
Ashok <c00540105@gmail.com>
2026-03-14 15:54:46 -0500

    Hello World

--- 06-variables.sh
+++ 06-variables.sh
@@ -3,3 +3,8 @@
 TIMESTAMP=$(date)
 
 echo "Script executed at: $TIMESTAMP"
+
+sleep 10
+END_TIMESTAMP=$(date)
+TOTAL_TIME=$(( $(date -d "$END_TIMESTAMP" +%s) - $(date -d "$TIMESTAMP" +%s) ))
+echo "Script completed after $TOTAL_TIME seconds."

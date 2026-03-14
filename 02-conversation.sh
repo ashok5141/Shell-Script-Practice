33d04c972b08fb64318c812095131c9b7e05a6b0
Ashok <c00540105@gmail.com>
2026-03-14 15:33:59 -0500

    Hello World

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Suresh: Hello Ramesh" How are you?
+echo "Ramesh: Hi Suresh, I am fine. How are you?"
+echo "Suresh: I am doing good. What are you doing these days?"
+echo "Ramesh: I am learning Shell Scripting. It is very interesting."
+echo "Suresh: That's great! Keep it up."    

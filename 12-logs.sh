48da76d5893888bdef1d86d7573c2a01aae2eb80
Ashok <c00540105@gmail.com>
2026-03-14 19:06:28 -0500

    Installing Nginx

---
+++ 12-logs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script as root user access."
+    exit 1
+fi
+
+# By default shell will not execute, only execute when called
+VALIDATE() {
+    if [ $? -eq 0 ]; then
+        echo "$1 installed . . . SUCCESS"
+    else
+        echo "$1 installation . . . FAILURE"
+        exit 1
+    fi
+}
+
+echo "Installing Nginx web server..."
+dnf install nginx -y &>> $LOGS_FILE
+VALIDATE "Nginx"
+
+
+dnf install mysql -y &>>
+VALIDATE "MySQL"
+
+dnf install nodejs -y &>> $LOGS_FILE
+VALIDATE "Node.js"

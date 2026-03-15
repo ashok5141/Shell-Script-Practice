d442238b75bf4919ba3d038238e4cdb56031664e
Ashok <c00540105@gmail.com>
2026-03-14 19:13:12 -0500

    Logs file during installation

--- 12-logs.sh
+++ 12-logs.sh
@@ -4,16 +4,16 @@ USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
 if [ $USERID -ne 0 ]; then
-    echo "Please run this script as root user access."
+    echo "Please run this script as root user access." | tee -a $LOGS_FILE
     exit 1
 fi
 
 # By default shell will not execute, only execute when called
 VALIDATE() {
     if [ $? -eq 0 ]; then
-        echo "$1 installed . . . SUCCESS"
+        echo "$1 installed . . . SUCCESS" | tee -a $LOGS_FILE
     else
-        echo "$1 installation . . . FAILURE"
+        echo "$1 installation . . . FAILURE" | tee -a $LOGS_FILE
         exit 1
     fi
 }

d59696b74232d213f53ffe482291626f4ce5a508
Ashok <c00540105@gmail.com>
2026-03-21 17:26:12 -0500

    Loops

---
+++ 15-loops.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script as root user access." | tee -a $LOGS_FILE
+    exit 1
+fi
+
+
+mkdir -p $LOGS_FOLDER
+
+# By default shell will not execute, only execute when called
+VALIDATE() {
+    if [ $1 -eq 0 ]; then
+        echo "$2  . . . SUCCESS" | tee -a $LOGS_FILE
+    else
+        echo "$2  . . . FAILURE" | tee -a $LOGS_FILE
+        exit 1
+    fi
+}
+
+for package in $@ # sudo bash 14-loops.sh nginx mysql nodejs
+do
+    dnf list installed $package &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
+        echo "$package not installed, installing now." | tee -a $LOGS_FILE
+        dnf install $package -y &>>$LOGS_FILE
+        VALIDATE $? "$package installation" 
+    else
+        echo "$package is already installed, skipping." | tee -a $LOGS_FILE
+    fi
+done
